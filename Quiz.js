df1f2839bec2fcb9297afb6489d1579ed9e558c6
hansulab <hansu1115@gmail.com>
2022-03-17 15:40:59 +0900

    퀴즈 추가

---
+++ Quiz.js
@@ -0,0 +1,49 @@
+const qz = {};
+
+qz.ManQuizImage = [
+    "", //0
+    "https://i.imgur.com/VoY3QCb.jpeg", //1
+    "https://i.imgur.com/vi6uQYx.jpeg", //2
+    "https://i.imgur.com/lkNQxSZ.jpeg", //3
+    "https://i.imgur.com/kEUnkNX.jpeg", //4
+    "https://i.imgur.com/Eo9sULM.jpeg", //5
+    "", //6
+    "", //7
+    "", //8
+    "", //9
+    "", //10
+    "", //11
+    "", //12
+    "", //13
+    "", //14
+    "", //15
+    "", //16
+    "", //17
+    "", //18
+    "", //19
+];
+
+qz.ManQuizAnswer = [
+    "", //0
+    "김수현", //1
+    "이병헌", //2
+    "황정민", //3
+    "주지훈", //4
+    "유해진", //5
+    "", //6
+    "", //7
+    "", //8
+    "", //9
+    "", //10
+    "", //11
+    "", //12
+    "", //13
+    "", //14
+    "", //15
+    "", //16
+    "", //17
+    "", //18
+    "", //19
+];
+
+module.exports = qz;
